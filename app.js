25c728bdb322ab9d6ddfd4e78e6dc961da1cb3a6
hoang khoi <hoangkhoik5@gmail.com>
2017-07-19 11:02:37 +0700

    intial 3

--- app.js
+++ app.js
@@ -1,3 +1,71 @@
+/***********************
+ * Module dependencies.
+ **********************/
+ const http = require('http');
+ const express = require('express');
+ const app = express();
+ const debug = require('debug')('webapp:server');
 console.log ("------------------------");
-console.log('llllllllllllllll');
+console.log('this');
 console.log ("------------------------");
+
+var port = 4000;
+app.set('port', port);
+
+const server = http.createServer(app);
+
+// Listen on provided port, on all network interfaces.
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+  ? 'Pipe ' + port
+  : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+    console.error(bind + ' requires elevated privileges');
+    process.exit(1);
+    break;
+    case 'EADDRINUSE':
+    console.error(bind + ' is already in use');
+    process.exit(1);
+    break;
+    default:
+    throw error;
+  }
+}
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+  ? 'pipe ' + addr
+  : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
+module.exports = app;
